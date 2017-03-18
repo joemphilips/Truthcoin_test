0a5e9a96b6e60a9f1188b6588de962c9add4f2d9
joemphilips <joemphilips@gmail.com>
2017-03-18 23:17:36 +0900

    add publishing script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -u
+
+readonly VERSION="1.0"
+readonly SCRIPT_DIR_PATH=$(dirname $(readlink -f $0))
+
+
+ipython nbconvert --to html data/Truthcoin_memo.ipynb --stdout > docs/Truthcoin_memo.html
+
