a964b66176a6b351284bf5ce0ffa5a5cc0c6ccd4
joemphilips <joemphilips@gmail.com>
2017-03-29 01:29:18 +0900

    update

--- build.sh
+++ build.sh
@@ -4,8 +4,11 @@ set -u
 readonly VERSION="1.0"
 readonly SCRIPT_DIR_PATH=$(dirname $(readlink -f $0))
 
+for note in `ls data/*.ipynb`; do
+    jupyter nbconvert --to html $note --stdout > docs/`basename ${note} .ipynb`.html;
+done
 
-jupyter nbconvert --to html data/Truthcoin_memo.ipynb --stdout > docs/Truthcoin_memo.html
-jupyter nbconvert --to html data/LMSR_function_demo.ipynb --stdout > docs/LMSR_function_demo.html
+# jupyter nbconvert --to html data/Truthcoin_memo.ipynb --stdout > docs/Truthcoin_memo.html
+# jupyter nbconvert --to html data/LMSR_function_demo.ipynb --stdout > docs/LMSR_function_demo.html
 cp -r data/images docs
 git push
